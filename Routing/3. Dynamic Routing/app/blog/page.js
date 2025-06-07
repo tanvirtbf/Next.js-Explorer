4acb2c0f8b22a80aab4f12ee807e3ac4edb4d050
tanvirtbf <tanver018765@gmail.com>
2025-06-07 19:38:49 +0600

    searchParams test with Blog page and with destructuring

--- Routing/3. Dynamic Routing/app/blog/page.js
+++ Routing/3. Dynamic Routing/app/blog/page.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const Blog = () => {
+const Blog = async ({params, searchParams}) => {
+    console.log(await params);
+    console.log(await searchParams);
   return (
     <div>Blog</div>
   )
