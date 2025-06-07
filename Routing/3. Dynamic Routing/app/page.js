569f6bbd52413b8478af34a1ae474122d3caee54
tanvirtbf <tanver018765@gmail.com>
2025-06-07 19:35:50 +0600

    searchParams test with some promise way

--- Routing/3. Dynamic Routing/app/page.js
+++ Routing/3. Dynamic Routing/app/page.js
@@ -1,6 +1,12 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home(props) {
+  console.log(await props.params);
+  console.log(await props.searchParams);
+
+  // if route have some query parameter like route/?name=tanvir&age=25 then...
+  console.log(await props.searchParams); // {name: 'tanvir', age: '25'}
+  
   return (
     <>
       <h1>Hello, This is Home Page!</h1>
