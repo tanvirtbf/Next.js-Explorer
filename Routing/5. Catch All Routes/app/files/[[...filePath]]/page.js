8b9d8d446eae1211b6aaf019921dddc164df0568
tanvirtbf <tanver018765@gmail.com>
2025-06-07 21:37:11 +0600

    required catch all route and optional catch all route

--- Routing/5. Catch All Routes/app/files/[[...filePath]]/page.js
+++ Routing/5. Catch All Routes/app/files/[[...filePath]]/page.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const FilePath = async ({params}) => {
     console.log(await params);
+    const {filePath} = await params
   return (
-    <div>FilePath</div>
+    <div>FilePath : {filePath?.join("/")}</div>
   )
 }
 
