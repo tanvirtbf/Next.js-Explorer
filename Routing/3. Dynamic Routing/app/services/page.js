e182d7830772ebc1024a4963cf36f473a7b19c4e
tanvirtbf <tanver018765@gmail.com>
2025-06-07 19:27:37 +0600

    understand dynamic routing deeply with promises

---
+++ Routing/3. Dynamic Routing/app/services/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+        <h1>Services</h1>
+        <Link href={"/"}>Home</Link>
+    </div>
+  )
+}
+
+export default Services
