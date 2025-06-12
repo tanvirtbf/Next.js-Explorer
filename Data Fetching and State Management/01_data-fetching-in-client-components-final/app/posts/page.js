a4170757e5d1fd9dc16b3c8ec312f795976021bf
tanvirtbf <tanver018765@gmail.com>
2025-06-12 16:45:11 +0700

    test fetching in client component

---
+++ Data Fetching and State Management/01_data-fetching-in-client-components-final/app/posts/page.js
@@ -0,0 +1,19 @@
+
+const Posts = () => {
+
+  return (
+    <>
+      <h1>Posts</h1>
+      {/* <div className="posts-container">
+        {posts.map(({ id, title, body }) => (
+          <div className="post-card" key={id}>
+            <h2>{title}</h2>
+            <p>{body}</p>
+          </div>
+        ))}
+      </div> */}
+    </>
+  );
+};
+
+export default Posts;
