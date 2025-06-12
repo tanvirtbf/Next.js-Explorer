616dd188f1e962d2e8825bebef539af9f1d15aa3
tanvirtbf <tanver018765@gmail.com>
2025-06-12 17:03:08 +0700

    test data fetching in csr

--- Data Fetching and State Management/01_data-fetching-in-client-components-final/app/posts/page.js
+++ Data Fetching and State Management/01_data-fetching-in-client-components-final/app/posts/page.js
@@ -1,9 +1,30 @@
+"use client"
+import { useEffect, useState } from "react";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() =>{
+    async function getData(){
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+      const data = await response.json()
+      console.log(data);
+      setPosts(data)
+    }
+    getData()
+  }, [])
 
   return (
     <>
       <h1>Posts</h1>
+      <div className="posts-container">
+        {posts.map(({id, title, body}) => (
+          <div className="post-card" key={id}>
+            <h2>{title}</h2>
+            <p>{body}</p>
+          </div>
+        ))}
+      </div>
       {/* <div className="posts-container">
         {posts.map(({ id, title, body }) => (
           <div className="post-card" key={id}>
