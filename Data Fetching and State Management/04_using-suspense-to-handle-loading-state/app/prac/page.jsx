c9915d0bd11ed9673f839ce16ae99f914a0978cd
tanvirtbf <tanver018765@gmail.com>
2025-06-15 10:16:47 +0600

    data streaming test

--- Data Fetching and State Management/04_using-suspense-to-handle-loading-state/app/prac/page.jsx
+++ Data Fetching and State Management/04_using-suspense-to-handle-loading-state/app/prac/page.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+
+const ComOne = lazy(() => import("../../components/prac/ComOne"))
+const ComTwo = lazy(() => import("../../components/prac/ComTwo"))
 
 const Prac = async () => {
-
   return (
     <div>
       <h1>Prac Page</h1>
-      <h2>{data1.message}</h2>
-      <h2>{data2.message}</h2>
+      <Suspense fallback={<div>Loading ComOne...</div>}>
+        <ComOne />
+      </Suspense>
+      <Suspense fallback={<div>Loading ComTwo...</div>}>
+        <ComTwo />
+      </Suspense>
     </div>
   );
 };
