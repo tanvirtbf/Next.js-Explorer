1030ad734e25078b77b61e2375923a318132392e
tanvirtbf <tanver018765@gmail.com>
2025-06-15 09:22:56 +0600

    test with Promise.all

--- Data Fetching and State Management/04_using-suspense-to-handle-loading-state/app/prac/page.jsx
+++ Data Fetching and State Management/04_using-suspense-to-handle-loading-state/app/prac/page.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Prac = async () => {
-    const res = await fetch("https://procodrr.vercel.app/?sleep=2000")
-    const data1 = await res.json()
+    // const res = await fetch("https://procodrr.vercel.app/?sleep=2000")
+    // const data1 = await res.json()
 
-    const res2 = await fetch("https://procodrr.vercel.app/?sleep=3000")
-    const data2 = await res2.json()
+    // const res2 = await fetch("https://procodrr.vercel.app/?sleep=3000")
+    // const data2 = await res2.json()
+
+    const [res, res2] = await Promise.all([fetch("https://procodrr.vercel.app/?sleep=2000"), fetch("https://procodrr.vercel.app/?sleep=3000")])
+    const [data1, data2] = await Promise.all([res.json(), res2.json()])
 
   return (
     <div>
