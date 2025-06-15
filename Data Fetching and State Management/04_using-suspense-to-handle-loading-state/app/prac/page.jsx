446d4cbbcde69e4ff522d38a83997ed0d740d9eb
tanvirtbf <tanver018765@gmail.com>
2025-06-15 09:38:03 +0600

    simplify data fetching

--- Data Fetching and State Management/04_using-suspense-to-handle-loading-state/app/prac/page.jsx
+++ Data Fetching and State Management/04_using-suspense-to-handle-loading-state/app/prac/page.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from "react";
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
 
 const Prac = async () => {
-    // const res = await fetch("https://procodrr.vercel.app/?sleep=2000")
-    // const data1 = await res.json()
-
-    // const res2 = await fetch("https://procodrr.vercel.app/?sleep=3000")
-    // const data2 = await res2.json()
-
-    const [res, res2] = await Promise.all([fetch("https://procodrr.vercel.app/?sleep=2000"), fetch("https://procodrr.vercel.app/?sleep=3000")])
-    const [data1, data2] = await Promise.all([res.json(), res2.json()])
+  const [data1, data2] = await Promise.all([
+    fetchData("https://procodrr.vercel.app/?sleep=2000"),
+    fetchData("https://procodrr.vercel.app/?sleep=3000"),
+  ]);
 
   return (
     <div>
-        <h1>Prac Page</h1>
-        <h2>{data1.message}</h2>
-        <h2>{data2.message}</h2>
+      <h1>Prac Page</h1>
+      <h2>{data1.message}</h2>
+      <h2>{data2.message}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Prac
+export default Prac;
