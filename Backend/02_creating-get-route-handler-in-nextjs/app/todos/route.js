3e8c547c32765196297e8b7b9634146ed1886cb8
tanvirtbf <tanver018765@gmail.com>
2025-06-17 12:32:01 +0600

    get todos data

--- Backend/02_creating-get-route-handler-in-nextjs/app/todos/route.js
+++ Backend/02_creating-get-route-handler-in-nextjs/app/todos/route.js
@@ -1,9 +1,14 @@
+export function GET() {
+  const todosData = [
+    { id: 1, text: "Learn Next.js 15", completed: false },
+    { id: 2, text: "Master Node.js", completed: true },
+    { id: 3, text: "Learn MongoDB", completed: true },
+  ];
 
-export function GET(){
-  console.log('Running GET request in todos route');
-  return new Response(JSON.stringify({ message: 'Hello from todos route' }), {
+  console.log("Running GET request in todos route");
+  return new Response(JSON.stringify(todosData), {
     headers: {
-      'Content-Type': 'application/json',
-    }
-  })
+      "Content-Type": "application/json",
+    },
+  });
 }
