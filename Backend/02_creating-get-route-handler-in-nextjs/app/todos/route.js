3462028bf1428a5c8c22c78932dcad68c92454ee
tanvirtbf <tanver018765@gmail.com>
2025-06-17 12:31:08 +0600

    Basic get route create

--- Backend/02_creating-get-route-handler-in-nextjs/app/todos/route.js
+++ Backend/02_creating-get-route-handler-in-nextjs/app/todos/route.js
@@ -1,13 +1,9 @@
-import todosData from "../../todos";
 
-export function GET() {
-  return Response.json(todosData);
-
-  //   return new Response(JSON.stringify(todosData), {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     status: 200,
-  //     statusText: "ProCodrr",
-  //   });
+export function GET(){
+  console.log('Running GET request in todos route');
+  return new Response(JSON.stringify({ message: 'Hello from todos route' }), {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
 }
