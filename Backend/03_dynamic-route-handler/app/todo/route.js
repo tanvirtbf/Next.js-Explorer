8ecaf4e5464f01fd1d0c2427181214c55dfc060f
tanvirtbf <tanver018765@gmail.com>
2025-06-24 10:57:02 +0600

    todos data expend

--- Backend/03_dynamic-route-handler/app/todo/route.js
+++ Backend/03_dynamic-route-handler/app/todo/route.js
@@ -1,5 +1,15 @@
+const todosData = [
+  { id: 1, text: "Learn Next.js 15", completed: false },
+  { id: 2, text: "Master Node.js", completed: true },
+  { id: 3, text: "Learn MongoDB", completed: true },
+  { id: 4, text: "Learn Express.js", completed: false },
+  { id: 5, text: "Learn React.js", completed: true },
+  { id: 6, text: "Learn Mysql", completed: false },
+];
+
+
 export function GET(req, res){
-    return new Response(JSON.stringify({name: "Tanvir Ahmed", age:25, email: "tanvirahmed@gmail.com"}), {
+    return new Response(JSON.stringify(todosData), {
         headers: {
             "Content-Type": "application/json"
         },
