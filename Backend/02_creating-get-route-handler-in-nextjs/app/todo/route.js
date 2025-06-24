092fd9851343f25d015f84760fdede77d732248d
tanvirtbf <tanver018765@gmail.com>
2025-06-24 10:24:49 +0600

    udpate

--- Backend/02_creating-get-route-handler-in-nextjs/app/todo/route.js
+++ Backend/02_creating-get-route-handler-in-nextjs/app/todo/route.js
@@ -1,11 +1,16 @@
+const todosData = [
+  { id: 1, text: "Learn Next.js 15", completed: false },
+  { id: 2, text: "Master Node.js", completed: true },
+  { id: 3, text: "Learn MongoDB", completed: true },
+];
+
+
 export function GET(req, res) {
     console.log('Running Get Rounte Handler');
-    return new Response(JSON.stringify({message: "Hello from GET Route Handler"}), {
+    return new Response(JSON.stringify(todosData), {
         headers: {
-            // "Content-Type": "application/json",
-            "Content-Type": "text/plain",
-
+            "Content-Type": "application/json",
         }
-    })
+    });
 }
 
