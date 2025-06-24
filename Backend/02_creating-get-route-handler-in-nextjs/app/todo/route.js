e6bd423cc32a4d06ee4324924e562d218616f231
tanvirtbf <tanver018765@gmail.com>
2025-06-24 09:54:11 +0600

    update

--- Backend/02_creating-get-route-handler-in-nextjs/app/todo/route.js
+++ Backend/02_creating-get-route-handler-in-nextjs/app/todo/route.js
@@ -1,5 +1,11 @@
 export function GET(req, res) {
     console.log('Running Get Rounte Handler');
-    return new Response(JSON.stringify({message: "Hello from GET Route Handler"}))
+    return new Response(JSON.stringify({message: "Hello from GET Route Handler"}), {
+        headers: {
+            // "Content-Type": "application/json",
+            "Content-Type": "text/plain",
+
+        }
+    })
 }
 
