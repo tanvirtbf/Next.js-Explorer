be09ecdb4fca9c36948e6b8465bae2e30c1ea374
tanvirtbf <tanver018765@gmail.com>
2025-06-24 11:08:37 +0600

    bug fixing on..

--- Backend/03_dynamic-route-handler/app/todo/[id]/route.js
+++ Backend/03_dynamic-route-handler/app/todo/[id]/route.js
@@ -1,4 +1,23 @@
+const todosData = [
+  { id: 1, text: "Learn Next.js 15", completed: false },
+  { id: 2, text: "Master Node.js", completed: true },
+  { id: 3, text: "Learn MongoDB", completed: true },
+  { id: 4, text: "Learn Express.js", completed: false },
+  { id: 5, text: "Learn React.js", completed: true },
+  { id: 6, text: "Learn Mysql", completed: false },
+];
+
+
 export async function GET(req, res){
-    console.log(await res.params.id);
-    return new Response("Hello, Next.js!");
+    const param= await res.params.id
+    for(let i=0; i<todosData.length; i++){
+        if(todosData[i].id===param){
+            return new Response(JSON.stringify(todosData[Number(param)]), {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+        }
+    }
+    return Response.json({message: "Not Found"});
 }
