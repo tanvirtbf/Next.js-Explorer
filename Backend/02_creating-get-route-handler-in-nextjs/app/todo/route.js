544a369676b2e03fed248784d3252f247aef1658
tanvirtbf <tanver018765@gmail.com>
2025-07-06 15:46:08 +0600

    Get Route complete

--- Backend/02_creating-get-route-handler-in-nextjs/app/todo/route.js
+++ Backend/02_creating-get-route-handler-in-nextjs/app/todo/route.js
@@ -2,9 +2,12 @@ import todosData from "../../todos.json";
 
 export async function GET(req, res) {
     console.log('Running Get Handler!');
-    return new Response(JSON.stringify(todosData), {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+    return Response.json(todosData);
+    // return new Response(JSON.stringify(todosData), {
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     status: 200,
+    //     statusText: "Hello World!"
+    // })
 }
