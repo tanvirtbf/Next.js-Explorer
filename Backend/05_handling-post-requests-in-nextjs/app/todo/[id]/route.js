3069a7e60f6e9e0df879dc6f73e0e77276416f0e
tanvirtbf <tanver018765@gmail.com>
2025-07-08 16:29:06 +0600

    put method explore

--- Backend/05_handling-post-requests-in-nextjs/app/todo/[id]/route.js
+++ Backend/05_handling-post-requests-in-nextjs/app/todo/[id]/route.js
@@ -1,8 +1,16 @@
+import { writeFile } from "node:fs/promises";
 import todos from '../../../todos.json'
 
-export async function PATCH(req, res){
-    const { id } = await req.params;
+export async function PUT(req, res){
     const editableTodo = await req.json();
+    const {id} = await res.params;
 
-    return new Response(JSON.stringify({todos: 1}));
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+
+    const todo = todos[todoIndex]
+
+    todos[todoIndex] = editableTodo;
+    await writeFile('./todos.json', JSON.stringify(todos));
+
+    return new Response(JSON.stringify(todo));
 }
