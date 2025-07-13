064971aed9f2eec95f18febcf1632c5315dfd62d
tanvirtbf <tanver018765@gmail.com>
2025-07-13 11:08:29 +0600

    Database Connected

--- Nextjs With MongoDB/01_connecting-mongodb/lib/connectDB.js
+++ Nextjs With MongoDB/01_connecting-mongodb/lib/connectDB.js
@@ -2,17 +2,10 @@ import mongoose from "mongoose";
 
 const DB_URI = "mongodb+srv://root:root@cluster0.klfbmop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-export const connectDB = async () => {
-  try {
-    if (mongoose.connection.readyState === 1) {
-      console.log("Already connected!");
-      return;
-    }
-    await mongoose.connect(DB_URI);
-    console.log("Database connected!");
-  } catch (err) {
-    console.log(err);
-    console.log("Database not connected!");
-    process.exit(1);
-  }
-};
+await mongoose.connect(DB_URI);
+
+const db = mongoose.connection.db;
+
+clg("Database Connected!");
+
+export default db;
