c774146471529c35b49afcfb5628320647c73339
tanvirtbf <tanver018765@gmail.com>
2025-07-15 17:33:10 +0600

    getLoggedInUser function created

--- Auth/04_implementing-login-user-in-nextjs/lib/auth.js
+++ Auth/04_implementing-login-user-in-nextjs/lib/auth.js
@@ -1,14 +1,23 @@
 import User from "@/models/userModel";
 import { cookies } from "next/headers";
 
-export async function getLoggedInUser(){
-    const cookieStore = await cookies();
-    const userId = cookieStore.get("userId")?.value;
-    const user = await User.findById(userId);
-    if(!user){
-        return Response.json(
-            {error: 'Please Login'},
-            {status: 401}
-        )
-    }
+export async function getLoggedInUser() {
+  const cookieStore = await cookies();
+  const userId = cookieStore.get("userId")?.value;
+
+  const errorResponse = Response.json(
+    { error: "Please Login" },
+    { status: 401 }
+  );
+
+  if (!userId) {
+    return errorResponse;
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return errorResponse;
+  }
+
+  return user;
 }
