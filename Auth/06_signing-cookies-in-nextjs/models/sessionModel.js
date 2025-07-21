7f32f5a299ff7c7199dfa7639466fd74345527c1
tanvirtbf <tanver018765@gmail.com>
2025-07-21 17:32:07 +0600

    session model created

---
+++ Auth/06_signing-cookies-in-nextjs/models/sessionModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Session =
+  mongoose.models.Session ||
+  mongoose.model("Session", {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+  });
+
+export default Session;
